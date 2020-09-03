f2fcf72e1b0ab27a5749a2bb083ef49411c5111f
Junhong-Kim <jhk1005jhk@gmail.com>
2020-09-03 19:49:48 +0900

    init

---
+++ src/main/java/com/wagoowagoo/fincat/FincatApplication.java
@@ -0,0 +1,13 @@
+package com.wagoowagoo.fincat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FincatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FincatApplication.class, args);
+	}
+
+}
